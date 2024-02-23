6baeeb7f21a898e44a2922218e6e6ac1cb28f674
L.J. Hanson <ljhanson@users.noreply.github.com>
2024-02-23 13:10:11 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 #
 
-ARCH='amd64'; [ -n "$1" ] && ARCH="${1}"
+ARCH='arm64'; [ -n "$1" ] && ARCH="${1}"
 set -e
 
 IMG_TAG=$(./tag.sh)
